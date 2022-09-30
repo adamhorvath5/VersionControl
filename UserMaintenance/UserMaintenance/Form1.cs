6d882a73ece7aca6d044a35fcac2a8e1811ce675
i0kzyk <adam.horvath5@stud.uni-corvinus.hu>
2022-09-30 11:19:40 +0200

    folytatásra szoruló savefiledialog

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,7 @@ namespace UserMaintenance
             label1.Text = Resource1.FullName;
             
             button1.Text = Resource1.Add;
+            button2.Text = Resource1.Write;
 
             listBox1.DataSource = users;
             listBox1.ValueMember = "ID";
@@ -42,6 +44,17 @@ namespace UserMaintenance
                 
             };
             users.Add(u);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog save = new SaveFileDialog();
+            save.FileName = "alapertelmezett.txt";
+            save.Filter = "Text File I *.txt";
+            if (save.ShowDialog()==DialogResult.OK)
+                (
+                    StreamWriter writer = new StreamWriter(save.OpenFile()));
+            // innen folytatni
         }
     }
 }
