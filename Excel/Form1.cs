9973101fded70f1908782ef783013cc4fed75aa2
i0kzyk <adam.horvath5@stud.uni-corvinus.hu>
2022-10-07 11:07:07 +0200

    Excel munkafüzet létrehozása és megnyitása

--- Excel/Form1.cs
+++ Excel/Form1.cs
@@ -7,26 +7,54 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Excel = Microsoft.Office.Interop.Excel;
+using Excelke = Microsoft.Office.Interop.Excel;
 using System.Reflection;
 
 namespace Excel
+
 {
     public partial class Form1 : Form
     {
         RealEstateEntities context = new RealEstateEntities();
         List<Flat> Flats;
-        
+        Excelke.Application xlApp;
+        Excelke.Workbook xlWB;
+        Excelke.Worksheet xlSheet;
+
 
         public Form1()
         {
             InitializeComponent();
             LoadData();
+            CreateExcel();
         }
 
         private void LoadData()
         {
             Flats = context.Flats.ToList();
+        }
+
+        private void CreateExcel()
+        {
+            try
+            {
+                 xlApp = new Excelke.Application();
+                 xlWB = xlApp.Workbooks.Add(Missing.Value);
+                 xlSheet = xlWB.ActiveSheet;
+                 //CreateTable(); //később írjuk meg
+                 xlApp.Visible = true;
+                 xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
