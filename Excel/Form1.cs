c6a2b567d845c25defe81e7fb64af19418f62a7f
i0kzyk <adam.horvath5@stud.uni-corvinus.hu>
2022-10-07 10:41:34 +0200

    2-4. feladatok commitolása

--- Excel/Form1.cs
+++ Excel/Form1.cs
@@ -12,9 +12,24 @@ namespace Excel
 {
     public partial class Form1 : Form
     {
+        RealEstateEntities context = new RealEstateEntities();
+        List<Flat> Flats;
+        
+
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            Flats = context.Flats.ToList();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
         }
     }
 }
