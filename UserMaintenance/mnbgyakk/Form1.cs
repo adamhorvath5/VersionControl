2178c0274d96012114c761488c3b1eb9a907ab7a
i0kzyk <adam.horvath5@stud.uni-corvinus.hu>
2022-10-21 11:03:59 +0200

    7-es feladatig

--- UserMaintenance/mnbgyakk/Form1.cs
+++ UserMaintenance/mnbgyakk/Form1.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 
 namespace mnbgyakk
@@ -22,7 +23,7 @@ namespace mnbgyakk
             InitializeComponent();
             meghivas();
             dataGridView1.DataSource = Rates;
-            
+            chartRateData.DataSource = Rates;
         }
 
         private void meghivas()
@@ -61,6 +62,20 @@ namespace mnbgyakk
                 }
 
             }
+
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
         }
         
         
