8474b834ab643f77687454ec0e65ae449269ac5a
i0kzyk <adam.horvath5@stud.uni-corvinus.hu>
2022-11-25 10:49:51 +0100

    jelszó validálás

--- UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -10,13 +10,34 @@ namespace UnitTestExample.Test
 {
     public class AccountControllerTestFixture
     {
-        [Test]
-        private void TestValidateEmail(string email, bool expectedResult)
+        [Test,
+         TestCase("abcd1234", false),
+         TestCase("irf@uni-corvinus", false),
+         TestCase("irf.uni-corvinus.hu",false),
+         TestCase("irf@uni-corvinus.hu", true)
+        ]
+        public void TestValidateEmail(string email, bool expectedResult)
         {
             var acController = new Controllers.AccountController();
             var actualResult = acController.ValidateEmail(email);
             Assert.AreEqual(expectedResult, actualResult);
         }
+
+
+        [Test,
+            TestCase("abcd", false),
+            TestCase("ABCD1234", false),
+            TestCase("abcd1234", false),
+            TestCase("ab", false),
+            TestCase("AbCd1234", true)
+            ]
+        public void TestValidatePassword(string password, bool expectedResult)
+        {
+            var acController = new Controllers.AccountController();
+            var actualResult = acController.ValidatePassword(password);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
 
 
     }
