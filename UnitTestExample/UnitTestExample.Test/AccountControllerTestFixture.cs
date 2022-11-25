f64de34d9f5f2eecd7957cccb3b80a4467d2e682
i0kzyk <adam.horvath5@stud.uni-corvinus.hu>
2022-11-25 10:36:34 +0100

    tes függvény kreálása

--- UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,9 +11,13 @@ namespace UnitTestExample.Test
     public class AccountControllerTestFixture
     {
         [Test]
-        private void TestValidateEmail()
+        private void TestValidateEmail(string email, bool expectedResult)
         {
-
+            var acController = new Controllers.AccountController();
+            var actualResult = acController.ValidateEmail(email);
+            Assert.AreEqual(expectedResult, actualResult);
         }
+
+
     }
 }
