acbadbf04ca9296c63b47d4a316e49abd61c8183
i0kzyk <adam.horvath5@stud.uni-corvinus.hu>
2022-11-25 10:27:22 +0100

    újból, immáron a szüksége csomagok telepítése

---
+++ UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace UnitTestExample.Test
+{
+    public class AccountControllerTestFixture
+    {
+        [Test]
+        private void TestValidateEmail()
+        {
+
+        }
+    }
+}
