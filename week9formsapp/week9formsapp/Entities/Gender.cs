fca758059d0dd5e5e4e2aa8034f37c24508ebc55
i0kzyk <adam.horvath5@stud.uni-corvinus.hu>
2022-12-02 10:23:27 +0100

    osztályok létrehozása

---
+++ week9formsapp/week9formsapp/Entities/Gender.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week9formsapp.Entities
+{
+    public enum Gender
+    {
+        Male = 1,
+        Female = 2
+    }
+
+    Gender ádámNeme = Gender.Male;
+    Gender évaNeme = (Gender)2;
+
+
+}
